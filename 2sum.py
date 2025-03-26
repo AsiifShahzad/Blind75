6fe11fe79b6e677b34ae85b58a7378b62e209b0a
AsiifShahzad <151775466+AsiifShahzad@users.noreply.github.com>
2025-03-26 16:32:01 +0500

    Update 2sum.py

--- 2sum.py
+++ 2sum.py
@@ -2,9 +2,9 @@
 '''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in hashmap:
-                return [hashmap[diff], i]
-            hashmap[num] = i
+       hashmap={}
+       for index,value in enumerate(nums):
+        diff=target-value
+        if(diff in hashmap):
+            return [hashmap[diff],index]
+        hashmap[value]=index
